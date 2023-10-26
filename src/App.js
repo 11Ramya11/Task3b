201b0b9e0a235f79fa4d99a920731997d167c3eb
11Ramya11 <105539397+11Ramya11@users.noreply.github.com>
2023-10-26 19:27:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Task3b from './components/Task3b';
+
+function App() {
+  return (
+    <div className="App">
+      <Task3b/>
+    </div>
+  );
+}
+
+export default App;
